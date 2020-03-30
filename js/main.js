fe75eaa7cf2a3eed6efff108429484c2391a942e
CarlstonG <carlstongrefalde@gmail.com>
2020-03-30 13:23:31 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -1,38 +1,32 @@
-//select dom item UI structions tags etc.
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
+const menubtn = document.querySelector('.menu-btn');
+const menu= document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-items');
-//Main ini menu
+
+//Close and open menu
 
 let showMenu = false;
-menuBtn.addEventListener('click', toggleMenu);
+
+menubtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-if (!showMenu){
-                menuBtn.classList.add('close');
-                menu.classList.add('show');
-                menuNav.classList.add('show');
-                menuBranding.classList.add('show');
-                navItems.forEach(item => item.classList.add('show'));
+    if(!showMenu) {
+        menubtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
 
-                //menu state reset
-             showMenu = true;
-}else {
+        showMenu = true;
 
-                menuBtn.classList.remove('close');
-                menu.classList.remove('show');
-                menunNav.classList.remove('show');
-                menuBranding.classList.remove('show');
-                navItems.forEach(item => item.classList.remove('show'));
-    
-//set menu state
+    } else {
+        menubtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
 
-            showMenu = false;
-           
+        showMenu = false;
+    }
 }
-
-
-}
-
